ff230b1075f0ed85f722a59c77612864ead632cf
bfly2001 <brendan.sflynn@gmail.com>
2019-11-03 16:54:35 -0800

    Edited project cards and added each project modal.
    Edited project cards and added each project modal.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+function myFunction() {
+  $('#exampleModalCenter').modal('toggle');
+}
+
+function myGallery() {
+  $('#galleryModal').modal('toggle');
+}
+
+function myGame() {
+  $('#gameModal').modal('toggle');
+}
+
+function myDashboard() {
+  $('#dashboardModal').modal('toggle');
+}
+
+function myDirectory() {
+  $('#directoryModal').modal('toggle');
+}
