eb223cd586ec6eead813f8a1e60aeb0ef24c37f3
bfly2001 <brendan.sflynn@gmail.com>
2019-11-05 15:49:52 -0800

    Added navigation functionality to modal nav buttons.
    Added navigation functionality to modal nav buttons.

--- js/script.js
+++ js/script.js
@@ -18,3 +18,31 @@ function myDashboard() {
 function myDirectory() {
   $('#directoryModal').modal('toggle');
 }
+
+//modal navigation
+
+/*//Next Button for Modal
+modalNext.addEventListener('click', () => {
+  let index = document.querySelector('span').innerHTML;
+  index++;
+  console.log(index);
+  if(index >= 12) {
+    displayModal(0);
+  }
+  else if(index <= 12) {
+    displayModal(index);
+  }
+});
+//Previous Button for Modal
+modalBack.addEventListener('click', () => {
+  let index = document.querySelector('span').innerHTML;
+  if(index >= 1) {
+    index--;
+    console.log(index);
+    displayModal(index);
+  }
+  else {
+    console.log(index);
+    displayModal(11);
+  }
+});*/
